b528bc39de6aa1031e498997b3625d0a37ad7f33
beaprog <86886452+beaprog@users.noreply.github.com>
2024-04-09 20:32:40 +0530

    Update tt_um_example_PWM.v

--- src/tt_um_example_PWM.v
+++ src/tt_um_example_PWM.v
@@ -24,9 +24,8 @@ module tt_um_example (
   assign rst_n = 0;
     tt_um_PWM_Generator_Verilog tt_um_PWM_Generator_Verilog  (
     .clk(clk),
-    .reset(rst_n),
-	.ui_increase_duty(ui_in),
-        .ui_decrease_duty(ui_in),
+	    .ui_increase_duty(ui_in[0]),
+	    .ui_decrease_duty(ui_in[1]),
 	.uo_PWM_OUT(uo_out),
 	    
 );
